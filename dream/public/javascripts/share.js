01d1aaba90c041118077f5d1ef7508214c20d97d
panwei <wei.pan@jimubox.com>
2014-11-28 00:14:15 +0800

    enhance the dream app

--- dream/public/javascripts/share.js
+++ dream/public/javascripts/share.js
@@ -2,7 +2,7 @@ var left = $(".profile span").attr("data-left"),
 	data;
 
 data = {
-	'img': 'http://localhost:3001/images/bonus.png',
+	'img': 'http://localhost:3001/images/bonus.jpg?mobile_build_version',
   	'link': window.location.href,
   	'desc': '我还差' + left + '元就能领200元礼品卡了，赶快来帮我集资!',
   	'title': '优购时尚商城'
