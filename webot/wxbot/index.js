7197bc633b9057ffb37bff81fc9d31ae56ffcb6e
panwei <wei.pan@jimubox.com>
2014-11-28 23:28:57 +0800

    enhance ui

--- webot/wxbot/index.js
+++ webot/wxbot/index.js
@@ -35,17 +35,16 @@ module.exports = function(webot) {
                     info.text = '2014ACODEFORPRPLQ200FROMWEIXIN';
                     next();
                 } else {
-                    next(null, [{
-                        title: '找朋友集资换200元礼品卡',
-                        url: "http://mp.weixin.qq.com/s?__biz=MjM5NDA3MTk2MA==&mid=202566635&idx=1&sn=7a223d1e07e2f3e31ecec67e0e2152a9#rd",
-                        picUrl: 'https://mmbiz.qlogo.cn/mmbiz/T2w1owmCtYp6qWrtxkJsPBLhcgOkGnt1ZbSA52rVAtRD1v97jG87323YrvsQ79fDh5nZzFSwCslrskkgxekCkg/0',
-                        description: '找朋友集资，集满200元并成功领取礼品卡代码后可登陆优购时尚商城主页www.yougou.com进行购物',
-                    }, {
-                        title: '查看我的集资',
-                        url: conf.dream_root + '/dream/' + dream.id + '/grant',
-                        picUrl: 'https://mmbiz.qlogo.cn/mmbiz/T2w1owmCtYp6qWrtxkJsPBLhcgOkGnt1bM6IKzVicvs6RjrfoHQSZf8GBKnnb1cRG4W4xcAEyLaLp4w8FGyMuTQ/0',
-                        description: '查看我的集资',
-                    }]);
+                    var ruleUrl = "http://mp.weixin.qq.com/s?__biz=MjM5NDA3MTk2MA==&mid=202566635&idx=1&sn=7a223d1e07e2f3e31ecec67e0e2152a9#rd";
+                    var dreamUrl = conf.dream_root + '/dream/' + dream.id + '/grant'
+
+                    next(null, [
+                        "亲，您已经成功参加优购集资换200元礼品卡活动！您现在的集资是：\n",
+                        (dream.bonus/100).toFixed(2) + ' 元',
+                        "\n还差" + (200 - dream.bonus/100).toFixed(2) + '元就能领礼品卡了！赶快<a href="' + dreamUrl+ '">点击这里</a>，将募集页面发到朋友圈或者朋友！',
+                        '\n200张礼品卡，先到先得哦~~~\n',
+                        '了解更多活动规则请<a href="' + ruleUrl+ '">点击这里</a>'
+                    ].join("\n"));
                 }
             }, function(err) {
                 console.info(err);
