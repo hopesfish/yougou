f07a1cca8a537db8100d35007e3de66f02fbb85b
panwei <wei.pan@jimubox.com>
2014-12-06 11:02:38 +0800

    imporve ui

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
 // webot send --des http://117.121.50.27/wechat/webot --token yougouwx t 111
 
-// webot send --des http://localhost:8081/wechat/webot --token yougouwx t 111
+// webot send --des http://localhost:3000/wechat/webot --token yougouwx t 111
 
