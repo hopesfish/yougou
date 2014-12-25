2391c60a697ea81397a8bd2aa831be5b322800cd
panwei <wei.pan@jimubox.com>
2014-12-25 14:36:45 +0800

    end xmas

--- webot/wxbot/index.js
+++ webot/wxbot/index.js
@@ -61,13 +61,13 @@ module.exports = function(webot) {
                     info.text = '2014ACODEFORXMASFROMWEIXIN';
                     next();
                 } else {
-                    var over = false, url;
+                    var over = true, url;
 
                     if (over) {
                         url = 'http://mp.weixin.qq.com/s?__biz=MjM5NDA3MTk2MA==&mid=202995312&idx=1&sn=6527ac46c11b0869af262666dbab16de#rd';
                         return next(null, [
-                            '亲~很遗憾您来晚了一步，圣诞节的活动已经结束啦！元旦我们还会为您奉上更加精彩好玩的游戏哦！',
-                            '<a href="' + url + '">点击这里</a>了解优购，要关注我们哦！'
+                            '亲~很遗憾您来晚了一步，圣诞节的活动已经结束啦！元旦我们还会为您奉上更加精彩好玩的游戏哦！想要第一时间参与的话，请持续关注优购时尚商城微信公众号。我们将会不间断地奉上各种好玩的游戏+折扣给力的优惠信息给您哟，速度关注我们吧！了解更多请',
+                            '<a href="' + url + '">点击这里</a>。'
                         ].join(""));
                     } else {
                         url = conf.xmas_root + "/xmas/" + xmas.id + "/grant";
