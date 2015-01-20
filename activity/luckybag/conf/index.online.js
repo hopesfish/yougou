ed9445d7d19d8dacb885cb5ddca362e84f6f8faf
panwei <wei.pan@jimubox.com>
2015-01-20 22:57:03 +0800

    add conf for ali

---
+++ activity/luckybag/conf/index.online.js
@@ -0,0 +1,4 @@
+module.exports = {
+    api_root : 'http://117.121.50.27:8082',
+    server_root : 'http://121.42.153.68:8083'
+};
---
+++ shells/online/update.ali.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#stop
+#init
+mkdir /var/www
+mkdir /var/www/html
+mkdir /var/www/html/yougou-luckybag
+
+#code
+cd /home/yougou/yougou
+git reset --hard
+git pull
+
+cp -frap activity/luckybag/* /var/www/html/yougou-luckybag
+
+#update ngingx
+cd /home/yougou/yougou/shells/online/nginx/conf.d
+cp -f luckybag_ali.conf /etc/nginx/conf.d
+
+#777
+chown nginx:nginx -R /var/www/html/*
+
+#sed
+mobile_build_version=`date +%Y%m%d%s`
+sed -i "s#mobile_build_version#${mobile_build_version}#g" `grep -lr mobile_build_version /var/www/html/yougou-luckybag/*`
+
+sed -i "s#localhost:3001#121.42.153.68:8083#g" `grep -lr localhost:3001 /var/www/html/yougou-luckybag/*`
+
+cd /var/www/html/yougou-luckybag/public/stylesheets
+stylus --compress style.styl
+
+#npm update
+#forever start -l dream.log -a bin/www
+cd /var/www/html/yougou-luckybag
+rm conf/index.js
+mv conf/index.online.js conf/index.js
+#npm install
+#npm update
+pm2 bin/www
+
+service nginx start
