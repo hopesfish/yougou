c3509bcd5fa77ed897bebdf9b18374db2e6de843
panwei <wei.pan@jimubox.com>
2015-01-26 00:06:26 +0800

    add share js

--- activity/luckybag/public/javascripts/share.js
+++ activity/luckybag/public/javascripts/share.js
@@ -1,23 +1,26 @@
-var left = $(".profile span").attr("data-left"),
-	data;
-
-data = {
-	'img': 'http://localhost:3001/images/bonus.jpg?mobile_build_version',
-  	'link': window.location.href,
-  	'desc': '我还差' + left + '元就能领取200元购物卡了！可购买Nike/Adidas/新百伦/百丽/思加图/天美意/TATA等你来选购！',
-  	'title': ''
-};
-
-wechat('timeline', data, function() {});
-wechat('friend', data, function() {});
-wechat('showOptionMenu', function() {});  
-
-$().ready(function() {
-	$(".help").click(function() {
-		$(".mask").show();
-	});
-
-	$(".mask").click(function() {
-		$(this).hide();
-	});
+alert(1);
+var apiElement = $("#jsApi");
+wx.config({
+    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+    appId: apiElement.attr('appId'), // 必填，公众号的唯一标识
+    timestamp: apiElement.attr('timestamp'), // 必填，生成签名的时间戳
+    nonceStr: apiElement.attr('nonceStr'), // 必填，生成签名的随机串
+    signature: apiElement.attr('signature'),// 必填，签名，见附录1
+    jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+});
+wx.ready(function(){
+    wx.onMenuShareTimeline({
+        title: '小伙伴们~我正在优购时尚商城收集祝福赢取购物卡和品牌豪礼，速度支持我！', // 分享标题
+        link: window.location.href, // 分享链接
+        imgUrl: 'http://localhost:3001/images/share.jpg', // 分享图标
+        success: function () { 
+            // 用户确认分享后执行的回调函数
+        },
+        cancel: function () { 
+            // 用户取消分享后执行的回调函数
+        }
+    });
+});
+wx.error(function(res){
+    alert(res || '签名过期！');
 });
