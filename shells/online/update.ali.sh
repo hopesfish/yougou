71f7746425aecb018698407685569a6e6294f97d
panwei <wei.pan@jimubox.com>
2015-01-26 23:20:02 +0800

    fix shell

--- shells/online/update.ali.sh
+++ shells/online/update.ali.sh
@@ -24,9 +24,8 @@ chown nginx:nginx -R /var/www/html/*
 
 #sed
 mobile_build_version=`date +%Y%m%d%s`
-sed -i "s#mobile_build_version#${mobile_build_version}#g" `grep -lr mobile_build_version /var/www/html/yougou-luckybag/*`
-
-sed -i "s#localhost:3001#weixin.yougou.com/activity/luckybag#g" `grep -lr localhost:3001 /var/www/html/yougou-luckybag/*`
+sed -i "s#mobile_build_version#${mobile_build_version}#g" `grep -lr mobile_build_version /var/www/html/yougou-static/activity/*`
+sed -i "s#localhost:3001#weixin.yougou.com/activity/luckybag#g" `grep -lr localhost:3001 /var/www/html/yougou-static/activity/luckybag/*`
 
 cd /var/www/html/yougou-static/activity/luckybag/stylesheets
 stylus --compress style.styl
@@ -34,6 +33,7 @@ stylus --compress style.styl
 #npm update
 #forever start -l dream.log -a bin/www
 cd /var/www/html/yougou-luckybag
+rm -rf public
 rm -f conf/index.js
 mv -f conf/index.online.js conf/index.js
 #npm install
