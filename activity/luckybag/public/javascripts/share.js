e47bf86288e9afa29842cd98432fb93de4da4f50
panwei <wei.pan@jimubox.com>
2015-01-30 00:43:19 +0800

    change color

--- activity/luckybag/public/javascripts/share.js
+++ activity/luckybag/public/javascripts/share.js
@@ -22,7 +22,7 @@ wx.ready(function(){
     });
 
     wx.onMenuShareAppMessage({
-        title: '优购发红包啦！', // 分享标题 优购给您发新年红包啦！
+        title: '优购送你百万新年大礼包啦！', // 分享标题 优购给您发新年红包啦！
         desc: '小伙伴们~我正在优购时尚商城收集祝福赢取购物卡和品牌豪礼，速度支持我！', // 分享描述
         link: window.location.href, // 分享链接
         imgUrl: 'http://localhost:3001/images/share.jpg', // 分享图标
