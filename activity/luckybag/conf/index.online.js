25af7d53b947e59bf022f628719b2c216426fc78
panwei <wei.pan@jimubox.com>
2015-01-29 10:35:58 +0800

    fix config

--- activity/luckybag/conf/index.online.js
+++ activity/luckybag/conf/index.online.js
@@ -1,4 +1,5 @@
 module.exports = {
     api_root : 'http://117.121.50.27:8082',
-    server_root : 'http://121.42.153.68/activity/luckybag'
+    //server_root : 'http://121.42.153.68/activity/luckybag'
+    server_root : 'http://117.121.50.27:8083'
 };
