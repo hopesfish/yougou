155c6c6a16783e720b5af1762f2c7aca9b63dd61
panwei <wei.pan@jimubox.com>
2015-01-29 15:41:14 +0800

    change ip

--- webot/conf/index.online.js
+++ webot/conf/index.online.js
@@ -7,10 +7,10 @@ module.exports = {
         val : 1000 * 60 * 10
     },
     api_root : 'http://localhost:8082',
-    dream_root : 'http://117.121.50.27:8083',
-    xmas_root : 'http://117.121.50.27:8084',
+    dream_root : 'http://117.121.50.28:8083',
+    xmas_root : 'http://117.121.50.28:8084',
     //luckybag_root : 'http://121.42.153.68/activity/luckybag',
-    luckybag_root : 'http://117.121.50.27:8083',
+    luckybag_root : 'http://117.121.50.28:8083',
     salt : 'weexiao',
     weixin : 'yougouwx'
 };
