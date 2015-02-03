23714253af5fda6d23c1aa669dd0e3ec90dcbff6
panwei <wei.pan@jimubox.com>
2015-02-03 15:32:56 +0800

    add pic

--- activity/luckybag/public/javascripts/share.js
+++ activity/luckybag/public/javascripts/share.js
@@ -1,7 +1,7 @@
 var apiElement = $("#jsApi");
 
 wx.config({
-    debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
     appId: apiElement.attr('appId'), // 必填，公众号的唯一标识
     timestamp: apiElement.attr('timestamp'), // 必填，生成签名的时间戳
     nonceStr: apiElement.attr('nonceStr'), // 必填，生成签名的随机串
