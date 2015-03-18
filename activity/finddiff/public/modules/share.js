6d52993b19e8687ddacf91aef6ab2a76c21514ef
panwei <wei.pan@jimubox.com>
2015-03-18 23:59:17 +0800

    fix img

--- activity/finddiff/public/modules/share.js
+++ activity/finddiff/public/modules/share.js
@@ -27,7 +27,7 @@ wx.ready(function(){
         title: '优购版“找你妹”玩起来！', // 分享标题 优购给您发新年红包啦！
         desc: '选对优购在线品牌，300元现金礼品卡就是你哒！速度买Adidad/NIKE/NB去噜~', // 分享描述
         link: $("#share-link").attr('url'), // 分享链接
-        imgUrl: 'http://weixin.yougou.com/activity/finddiff/public/images/share.jpg?v=1', // 分享图标
+        imgUrl: 'http://weixin.yougou.com/activity/finddiff/public/images/share_9fe1c5f.jpg?v=1', // 分享图标
         type: 'link', // 分享类型,music、video或link，不填默认为link
         dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
         success: function () { 
