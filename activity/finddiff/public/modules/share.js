fc85a64f2b7653f92f71f38a10731893759fac5a
panwei <wei.pan@jimubox.com>
2015-03-21 17:18:45 +0800

    ui

--- activity/finddiff/public/modules/share.js
+++ activity/finddiff/public/modules/share.js
@@ -13,7 +13,7 @@ wx.ready(function(){
     wx.onMenuShareTimeline({
         title: '小伙伴们~我正在玩优购版“找你妹”，快来帮帮我！', // 分享标题
         link: $("#share-link").attr('url'), // 分享链接
-        imgUrl: 'http://weixin.yougou.com/activity/finddiff/share.jpg', // 分享图标
+        imgUrl: 'http://weixin.yougou.com/activity/finddiff/share.png', // 分享图标
         success: function () { 
             // 用户确认分享后执行的回调函数
         },
@@ -26,7 +26,7 @@ wx.ready(function(){
         title: '优购版“找你妹”玩起来！', // 分享标题 优购给您发新年红包啦！
         desc: '选对优购在线品牌，300元现金礼品卡就是你哒！速度买Adidad/NIKE/NB去噜~', // 分享描述
         link: $("#share-link").attr('url'), // 分享链接
-        imgUrl: 'http://weixin.yougou.com/activity/finddiff/share.jpg', // 分享图标
+        imgUrl: 'http://weixin.yougou.com/activity/finddiff/share.png', // 分享图标
         type: 'link', // 分享类型,music、video或link，不填默认为link
         dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
         success: function () { 
